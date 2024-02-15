214c3dafa99e4447ab1dc48d099180cf2288ba49
Mklfcqp <tomas.senti@seznam.cz>
2024-02-15 19:36:18 +0100

    update background

---
+++ src/components/TBackgroundGray.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="background">
+
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'TBackgroundGray'
+}    
+</script>
+
+
+<style scoped>
+.background {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100vh;
+    overflow: hidden;
+    background: #141414;
+}
+
+
+</style>
